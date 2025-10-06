47ba181c62114bf011f317d6ea10a74792a155bc
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-10-06 16:01:53 +0530

    Update load.py

--- load.py
+++ load.py
@@ -6,4 +6,4 @@ df = pd.read_csv("transformed.csv")
 
 # Simulate loading: copy file to "target.csv"
 shutil.copy("transformed.csv", "target.csv")
-print("✅ Load step completed. Data loaded into target.csv")
+print("Load step completed. Data loaded into target.csv")
